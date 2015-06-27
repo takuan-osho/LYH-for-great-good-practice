8b7434d3c2d23b567bb10312c4446408f0fcbe3c
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-06-27 10:00:24 +0900

    終わり。

---
+++ chap01/boom.hs
@@ -0,0 +1,12 @@
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+triples = [ (a,b,c) | c <- [1..10], a <- [1..10], b <- [1.10] ]
+
+rightTriangles = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2]
+
+rightTriangles' = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2, a+b+c == 24]
