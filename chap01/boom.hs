e5b54507162335d7bd15cce25900cc12cc71d93e
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-02-18 23:38:52 +0900

    hlintの設定に従う

--- chap01/boom.hs
+++ chap01/boom.hs
@@ -2,7 +2,7 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
-removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 triples = [ (a,b,c) | c <- [1..10], a <- [1..10], b <- [1.10] ]
